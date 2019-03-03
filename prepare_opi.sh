e3c88f6ac2e062633820998d93696c6e8a40b238
Roman Fedyashov <roman.fedyashov@gmail.com>
2019-03-03 08:49:27 +0000

    sdf

--- prepare_opi.sh
+++ prepare_opi.sh
@@ -16,3 +16,4 @@ sudo ./build
 echo "autologin"
 sudo cp -f lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp -f firmware.desktop /etc/xdg/autostart/firmware.desktop
+sudo echo "pi ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
